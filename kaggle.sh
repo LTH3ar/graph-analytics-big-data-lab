74d529333af26d637123a2692139c205fcf82a16
Akira Raven <letuanhuy1301@gmail.com>
2025-06-09 01:31:24 +0700

    update

--- kaggle.sh
+++ kaggle.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 curl -L -o metr-la-dataset.zip\
   https://www.kaggle.com/api/v1/datasets/download/annnnguyen/metr-la-dataset
+
+unzip metr-la-dataset.zip -d metr-la-dataset
+
+mkdir -p logs
